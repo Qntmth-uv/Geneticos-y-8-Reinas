613770a88ec4148c4b1f01033eaaf6cce1ed2b2e
José Juan <Quantumdx2022@gmail.com>
2023-10-03 23:44:43 -0600

    Se creo la función mutación inversa, toma dos padres, genera de manera aleatoria dos indices (index_numbers), se crea una lista que contiene esos indices y otra que no (range_init). Posteriormente se crean dos hijos usando la inversion en ambos casos uno manteniendo los genes del padre/madre intactos e invertiendo los valores en la posicion de la madre/padre. Devuelve dos hijos.

--- mutacion.py
+++ mutacion.py
@@ -1,37 +1,28 @@
 import numpy as np
 
 
-def mutation(vatter: list, mutter: list, interchange: bool):
-    #np.random.seed(19209)
-    if (type(vatter) != list) or (type(mutter) != list):
-        return "type error"
-    if len(vatter) != len(mutter):
-        return "list do not have the same size!"
-    else:
-        if len(vatter) == 2:
-            return "the list is not length enougth, the len of the list must be 3 or bigger"
-        index_numbers = np.random.choice(range(0, len(vatter)), size=2, replace=False)
-        if interchange == True:
-            mc = mutter.copy()
-            for i in index_numbers:
-                mc[i] = vatter[i]
-            return mc, index_numbers
-        else:
-            range_index = [i for i in range(sorted(index_numbers)[1], sorted(index_numbers)[0])]
-            #Indices de los valores a modificar en el hijo
-            range_list = [i for i in range(0, len(vatter), 1)]
-            #Indices en total del jefe
-            sohn_1 = [0 for i in range(0, len(vatter), 1)]
-            sohn_2 = sohn_1.copy()
-            for i in range_list:
-                sohn_1[i] = mutter[i]
-                sohn_2[i] = vatter[i]
-            for j in range_index:
-                sohn_1[j] = vatter[j]
-                sohn_2[j] = mutter[j]
-            return sohn_1, sohn_2, range_index
+def mutacion_inversion(vatter: list, mutter: list):
+    index_numbers = np.random.choice(range(0, len(vatter)), size=2, replace=False)
+    range_index = [i for i in range(sorted(index_numbers)[0], sorted(index_numbers)[1], 1)]
+    range_index_invers = sorted(range_index, reverse=True)
+    range_init = [i for i in range(0, sorted(index_numbers)[0], 1)]
+    range_end = [i for i in range(sorted(index_numbers)[1], len(mutter), 1)]
+    range_init.extend(range_end)
+    if 0 in index_numbers and 0 in range_init:
+        range_index.remove(0)
+    sohn_1 = [0 for i in range(0, len(vatter), 1)]
+    sohn_2 = sohn_1.copy()
+    for j in range_init:
+        sohn_1[j] = mutter[j]
+        sohn_2[j] = vatter[j]
+    for (i, j) in zip(range_index, range_index_invers):
+        sohn_1[i] = vatter[j]
+        sohn_2[i] = mutter[j]
+    return sohn_1, sohn_2
 
 
-a1 = np.random.randint(0, 100, size=10).tolist()
+#Ejemplos
+a1 = np.random.randint(0, 2, size=10).tolist()
 a2 = np.random.randint(0, 2, size=10).tolist()
-print(mutation(a1, a2, True))
+print(a1, "\n", a2)
+print(mutacion_inversion(a1, a2))
