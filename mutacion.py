59e34ad73de82e16914b1026792f72b72786001a
José Juan <Quantumdx2022@gmail.com>
2023-10-04 00:08:52 -0600

    No me habia percatado que la mutación aplica sobre un único elemento de la población, así que basandome en la mutacion_inversa creé una nueva mutación que permite la inversión. Intente usar la mutacion_inversa para crear la mutacion_uniparental pero me surgia un error que no logré detectar así que mejor decidí crear una nueva función.

--- mutacion.py
+++ mutacion.py
@@ -18,11 +18,29 @@ def mutacion_inversion(vatter: list, mutter: list):
     for (i, j) in zip(range_index, range_index_invers):
         sohn_1[i] = vatter[j]
         sohn_2[i] = mutter[j]
-    return sohn_1, sohn_2
+    return sohn_1, sohn_2, range_init, range_index
+
+
+def mutacion_uniparental(individuo: list):
+    index_numbers = np.random.choice(range(0, len(individuo)), size=2, replace=False)
+    range_index = [i for i in range(sorted(index_numbers)[0], sorted(index_numbers)[1], 1)]
+    range_index_invers = sorted(range_index, reverse=True)
+    range_init = [i for i in range(0, sorted(index_numbers)[0], 1)]
+    range_end = [i for i in range(sorted(index_numbers)[1], len(individuo), 1)]
+    range_init.extend(range_end)
+    if 0 in index_numbers and 0 in range_init:
+        range_index.remove(0)
+    mut = [0 for i in range(0, len(individuo), 1)]
+    for i in range_init:
+        mut[i] = individuo[i]
+    for (i,j) in zip(range_index, range_index_invers):
+        mut[i] = individuo[j]
+    return mut, range_init, range_index
 
 
 #Ejemplos
-a1 = np.random.randint(0, 2, size=10).tolist()
-a2 = np.random.randint(0, 2, size=10).tolist()
-print(a1, "\n", a2)
-print(mutacion_inversion(a1, a2))
+a1 = np.random.randint(100, 300, size=10).tolist()
+# a2 = np.random.randint(0, 2, size=10).tolist()
+print(a1)
+# print(mutacion_inversion(a1, a2))
+print(mutacion_uniparental(a1))
