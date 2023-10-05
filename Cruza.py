7ac751abeb621cc3e5bd6c5693e1ae9e146073a9
José Juan <Quantumdx2022@gmail.com>
2023-10-04 20:25:49 -0600

    Se logra recoger los index del primer ciclo.

--- Cruza.py
+++ Cruza.py
@@ -2,20 +2,21 @@ import numpy as np
 
 
 def cruza_ciclica(vatter: list, mutter: list):
+    if len(vatter) != len(mutter):
+        return "the list do not have the same size"
     index_init_v = [i for i in range(0, len(vatter), 1)]
     index_init_m = index_init_v.copy()
     valor_asociado_p = []
     valor_asociado_m = []
+    indices_tocados = []
     i = 0
-    while i in index_init_v:
+    while i in indices_tocados:
         asociado_p = vatter[i]
         asociado_m = mutter[i]
         valor_asociado_p.append(asociado_p)
         valor_asociado_m.append(asociado_m)
         index_asociado = vatter.index(asociado_m)
-        vatter.remove(asociado_p)
-        mutter.remove(asociado_m)
-        index_init_v.remove(i)
+        indices_tocados.append(i)
         i = index_asociado
     return valor_asociado_p
 
@@ -23,3 +24,23 @@ padrino_1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 madrina_1 = [9, 3, 7, 8, 2, 6, 5, 1, 4]
 
 print(cruza_ciclica(padrino_1,madrina_1))
+
+
+
+def ciclic(lista_1: list, lista_2: list):
+    i = 0
+    touchedIndex = []
+    while i not in touchedIndex:
+        index = i
+        indexValueList1 = lista_1[index]
+        indexValueList2 = lista_2[index]
+        SearchedIndexInList1 = lista_1.index(indexValueList2)
+        i = SearchedIndexInList1
+        touchedIndex.append(index)
+    for i in sorted(touchedIndex, reverse=True):
+        lista_1.pop(i)
+        lista_2.pop(i)
+    return touchedIndex, lista_1, lista_2
+
+
+print(ciclic(padrino_1,madrina_1))
