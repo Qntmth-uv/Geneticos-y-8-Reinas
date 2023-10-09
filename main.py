0f106593178bed777bd6b31c887364c1335f5b6b
José Juan <Quantumdx2022@gmail.com>
2023-10-09 00:47:55 -0600

    Función principal que une a todos los otros modulos

---
+++ main.py
@@ -0,0 +1,18 @@
+from remplazo_y_seleccion import *
+
+
+def eigthQueensProblem(populationSize: int, iter: int):
+    maxFitPerIter = []
+    pop_init = createPopulation(populationSize)
+    for i in range(0, iter, 1):
+        sel = selectionParents(pop_init)
+        if sel[1] == 0:
+            print(sel[0])
+        else:
+            maxFitPerIter.append(sel[1])
+            new_pop = remplacement(pop_init, sel[0])
+            pop_init = new_pop
+    return max(maxFitPerIter)
+
+print(eigthQueensProblem(50, 5))
+
