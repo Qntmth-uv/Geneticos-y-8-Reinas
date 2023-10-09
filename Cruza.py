702eee201660b07d3af9496b1ce5bf7e225d1c82
José Juan <Quantumdx2022@gmail.com>
2023-10-09 00:50:18 -0600

    Faltan terminar la cruza, se le asígno asi para que pudieran ser probadas las otras funciones.

--- Cruza.py
+++ Cruza.py
@@ -1,29 +1,29 @@
 import numpy as np
-
+from inicializacion import initTab
 
 def cruza_ciclica(vatter: list, mutter: list):
-    if len(vatter) != len(mutter):
-        return "the list do not have the same size"
-    index_init_v = [i for i in range(0, len(vatter), 1)]
-    index_init_m = index_init_v.copy()
-    valor_asociado_p = []
-    valor_asociado_m = []
-    indices_tocados = []
-    i = 0
-    while i in indices_tocados:
-        asociado_p = vatter[i]
-        asociado_m = mutter[i]
-        valor_asociado_p.append(asociado_p)
-        valor_asociado_m.append(asociado_m)
-        index_asociado = vatter.index(asociado_m)
-        indices_tocados.append(i)
-        i = index_asociado
-    return valor_asociado_p
+    # if len(vatter) != len(mutter):
+    #     return "the list do not have the same size"
+    # index_init_v = [i for i in range(0, len(vatter), 1)]
+    # index_init_m = index_init_v.copy()
+    # valor_asociado_p = []
+    # valor_asociado_m = []
+    # indices_tocados = []
+    # i = 0
+    # while i in indices_tocados:
+    #     asociado_p = vatter[i]
+    #     asociado_m = mutter[i]
+    #     valor_asociado_p.append(asociado_p)
+    #     valor_asociado_m.append(asociado_m)
+    #     index_asociado = vatter.index(asociado_m)
+    #     indices_tocados.append(i)
+    #     i = index_asociado
+    return initTab(), initTab()
 
-padrino_1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-madrina_1 = [9, 3, 7, 8, 2, 6, 5, 1, 4]
+# padrino_1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# madrina_1 = [9, 3, 7, 8, 2, 6, 5, 1, 4]
 
-print(cruza_ciclica(padrino_1,madrina_1))
+#print(cruza_ciclica(padrino_1,madrina_1))
 
 
 def ciclic(lista_1: list, lista_2: list):
@@ -45,4 +45,4 @@ def ciclic(lista_1: list, lista_2: list):
     return setTouchedIndex
 
 
-print(ciclic(padrino_1,madrina_1))
+#print(ciclic(padrino_1,madrina_1))
