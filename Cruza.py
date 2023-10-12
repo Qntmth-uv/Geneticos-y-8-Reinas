49c0fbdb64dd19279fd8e3c5e10ef23efbc2053c
José Juan <Quantumdx2022@gmail.com>
2023-10-12 13:55:30 -0600

    Se completo el modulo cruza.

--- Cruza.py
+++ Cruza.py
@@ -1,51 +1,5 @@
 from inicializacion import initTab
 
-def cruza_ciclica(vatter: list, mutter: list):
-    # if len(vatter) != len(mutter):
-    #     return "the list do not have the same size"
-    # index_init_v = [i for i in range(0, len(vatter), 1)]
-    # index_init_m = index_init_v.copy()
-    # valor_asociado_p = []
-    # valor_asociado_m = []
-    # indices_tocados = []
-    # i = 0
-    # while i in indices_tocados:
-    #     asociado_p = vatter[i]
-    #     asociado_m = mutter[i]
-    #     valor_asociado_p.append(asociado_p)
-    #     valor_asociado_m.append(asociado_m)
-    #     index_asociado = vatter.index(asociado_m)
-    #     indices_tocados.append(i)
-    #     i = index_asociado
-    return initTab(), initTab()
-
-padrino_1 = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-madrina_1 = [8, 2, 6, 7, 1, 5, 4, 0, 3]
-
-#print(cruza_ciclica(padrino_1,madrina_1))
-
-
-# def ciclic(lista_1: list, lista_2: list):
-#     setTouchedIndex = []
-#     while lista_1 != []:
-#         i = 0
-#         touchedIndex = []
-#         while i not in touchedIndex:
-#             index = i
-#             indexValueList1 = lista_1[index]
-#             indexValueList2 = lista_2[index]
-#             SearchedIndexInList1 = lista_1.index(indexValueList2)
-#             i = SearchedIndexInList1
-#             touchedIndex.append(index)
-#         for i in sorted(touchedIndex, reverse=True):
-#             lista_1.pop(i)
-#             lista_2.pop(i)
-#         setTouchedIndex.append(touchedIndex)
-#     return setTouchedIndex
-
-
-#print(ciclic(padrino_1,madrina_1))
-
 
 def cyclicV3(entity1: list, entity2: list):
     if len(entity1) != len(entity2):
@@ -76,8 +30,24 @@ def sexCyclic(entity1: list, entity2: list):
         genEntity1List.extend(i)
     for i in genEntity2:
         genEntity2List.extend(i)
-    return genEntity1List, genEntity2List
+    son = [0 for i in range(0, len(entity1), 1)]
+    for k in genEntity1List:
+        son[k] = entity1[k]
+    for j in genEntity2List:
+        son[j] = entity2[j]
+    return son
 
 
-print(sexCyclic(padrino_1, madrina_1))
+def biSonSex(entity1: list, entity2: list):
+    son1 = sexCyclic(entity1, entity2)
+    son2 = sexCyclic(entity2, entity1)
+    return son1, son2
+
+
+#TESTING
+#padrino_1 = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+#madrina_1 = [8, 2, 6, 7, 1, 5, 4, 0, 3]
 #print(cyclicV3(initTab(), initTab()))
+#print(sexCyclic(padrino_1, madrina_1))
+#print(biSonSex(initTab(), initTab()))
+
