413219d17eba2e46034371e21eec69677ca43a70
José Juan <Quantumdx2022@gmail.com>
2023-10-12 13:42:04 -0600

    Se terminaron los modulos de la cruza ciclica así mismo del metodo para generar los elementos hijos. Únicamente falta concluir el como se generan los hijos.

--- Cruza.py
+++ Cruza.py
@@ -1,4 +1,3 @@
-import numpy as np
 from inicializacion import initTab
 
 def cruza_ciclica(vatter: list, mutter: list):
@@ -20,29 +19,65 @@ def cruza_ciclica(vatter: list, mutter: list):
     #     i = index_asociado
     return initTab(), initTab()
 
-# padrino_1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-# madrina_1 = [9, 3, 7, 8, 2, 6, 5, 1, 4]
+padrino_1 = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+madrina_1 = [8, 2, 6, 7, 1, 5, 4, 0, 3]
 
 #print(cruza_ciclica(padrino_1,madrina_1))
 
 
-def ciclic(lista_1: list, lista_2: list):
-    setTouchedIndex = []
-    while lista_1 != []:
-        i = 0
-        touchedIndex = []
-        while i not in touchedIndex:
-            index = i
-            indexValueList1 = lista_1[index]
-            indexValueList2 = lista_2[index]
-            SearchedIndexInList1 = lista_1.index(indexValueList2)
-            i = SearchedIndexInList1
-            touchedIndex.append(index)
-        for i in sorted(touchedIndex, reverse=True):
-            lista_1.pop(i)
-            lista_2.pop(i)
-        setTouchedIndex.append(touchedIndex)
-    return setTouchedIndex
+# def ciclic(lista_1: list, lista_2: list):
+#     setTouchedIndex = []
+#     while lista_1 != []:
+#         i = 0
+#         touchedIndex = []
+#         while i not in touchedIndex:
+#             index = i
+#             indexValueList1 = lista_1[index]
+#             indexValueList2 = lista_2[index]
+#             SearchedIndexInList1 = lista_1.index(indexValueList2)
+#             i = SearchedIndexInList1
+#             touchedIndex.append(index)
+#         for i in sorted(touchedIndex, reverse=True):
+#             lista_1.pop(i)
+#             lista_2.pop(i)
+#         setTouchedIndex.append(touchedIndex)
+#     return setTouchedIndex
 
 
 #print(ciclic(padrino_1,madrina_1))
+
+
+def cyclicV3(entity1: list, entity2: list):
+    if len(entity1) != len(entity2):
+        return "sizes do not match!"
+    notouchedIndex = [i for i in range(0, len(entity1), 1)]
+    setOfToucedIndex = []
+    i = 0
+    while len(notouchedIndex) != 0:
+        touchedIndex = []
+        i = notouchedIndex[0]
+        while i in notouchedIndex:
+            associatedValueInEntt2 = entity2[i]
+            indexAsocciatedEntt1 = entity1.index(associatedValueInEntt2)
+            touchedIndex.append(i)
+            notouchedIndex.remove(i)
+            i = indexAsocciatedEntt1
+        setOfToucedIndex.append(touchedIndex)
+    return setOfToucedIndex
+
+
+def sexCyclic(entity1: list, entity2: list):
+    setofIndex = cyclicV3(entity1, entity2)
+    genEntity1 = [setofIndex[i] for i in range(0,len(setofIndex), 1) if (i % 2 == 0)]
+    genEntity2 = [setofIndex[i] for i in range(0, len(setofIndex), 1) if (i % 2 == 1)]
+    genEntity1List = []
+    genEntity2List = []
+    for i in genEntity1:
+        genEntity1List.extend(i)
+    for i in genEntity2:
+        genEntity2List.extend(i)
+    return genEntity1List, genEntity2List
+
+
+print(sexCyclic(padrino_1, madrina_1))
+#print(cyclicV3(initTab(), initTab()))
